37a953d8cc709525a29d937fc0dd7b1b5c7a8f2f
yar <yardenack@gmail.com>
2015-12-05 20:49:56 -0800

    dir for settings, db

--- index.js
+++ index.js
@@ -3,19 +3,30 @@
 var console = require('console');
 var irc = require('irc').Client;
 
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+        D: 'dir'
+    },
+    default: {
+        datadir: 'db',
+        dir: undefined
+    }
+});
+if (argv.dir === undefined) {
+    console.log("USAGE: bothead -D /path/to/bot/homedir");
+} else {
+    var set = require(argv.dir + '/settings.js');
+};
+
 var err = function(message) {
     console.log('error: ', message);
 };
 
-var username = '';
-var channels = '';
-var password = '';
-
-var client = new irc('chat.freenode.net', username, {
-    nick: username,
-    userName: username,
-    realName: username,
-    password: password,
+var client = new irc('chat.freenode.net', set.username, {
+    nick: set.username,
+    userName: set.username,
+    realName: set.username,
+    password: set.password,
     port: 7000,
     debug: true,
     showErrors: true,
@@ -36,9 +47,9 @@ var client = new irc('chat.freenode.net', username, {
 client.addListener('error', err);
 client.addListener('notice', function (nick, to, text, message) {
     if ((message.prefix === "NickServ!NickServ@services.")
-        && (to === username)
-        && (text === "You are now identified for " + username + ".")) {
-        client.join(channels);
+        && (to === set.username)
+        && (text === "You are now identified for " + set.username + ".")) {
+        client.join(set.channels);
     }
 });
 client.addListener('message', function (from, to, message) {
